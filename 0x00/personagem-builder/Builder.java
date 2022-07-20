7508b2811b21fb33797c33484e07041d2396e137
julianacit <julianamt@ciandt.com>
2022-07-20 10:36:13 -0300

    Task 01 - padrão de projeto Builder

---
+++ 0x00/personagem-builder/Builder.java
@@ -0,0 +1,9 @@
+public interface Builder {
+    public void setNome(String nome);
+    public void setTipoPersonagem(TipoPersonagem tipo);
+    public void setInteligencia(int inteligencia);
+    public void setForca(int forca);
+    public void setVigor(int vigor);
+    public void setResistencia(int resistencia);
+    public void setDestreza(int destreza);
+}
---
+++ 0x00/personagem-builder/LadraoBuilder.java
@@ -0,0 +1,35 @@
+public class LadraoBuilder implements Builder{
+    private String nome;
+    private TipoPersonagem tipo;
+    private int inteligencia;
+    private int forca;
+    private int vigor;
+    private int resistencia;
+    private int destreza;
+
+    public Ladrao build() {
+        return new Ladrao(nome, inteligencia, forca, vigor, resistencia, destreza);
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setTipoPersonagem(TipoPersonagem tipo) {
+        this.tipo = tipo;
+    }
+    public void setInteligencia(int inteligencia) {
+        this.inteligencia = inteligencia;
+    }
+    public void setForca(int forca) {
+        this.forca = forca;
+    }
+    public void setVigor(int vigor) {
+        this.vigor = vigor;
+    }
+    public void setResistencia(int resistencia) {
+        this.resistencia = resistencia;
+    }
+    public void setDestreza(int destreza) {
+        this.destreza = destreza;
+    }
+}
